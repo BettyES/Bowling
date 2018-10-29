7c83ad47ff1ca3cc2aa0c5ed7972d93022a8ad0c
bettyES <betty.schirrmeister@gmail.com>
2018-10-29 21:22:55 +0000

    implemented extreme case of all strikes (ending not set yet, will stop at frame 10)

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -1,20 +1,33 @@
-object Bowling{
+object Bowling {
 
   def main(args: Array[String]): Unit = {
     //points = scala.io.StdIn.readInt()
     var score1 = new Score()
     var points = 10
-    for(x<-1 to 10){
+    var stPrevFrame1 = "normal"
+    var stPrevFrame2 = "normal"
+    for (x <- 1 to 10) {
 
-      var frame1 = new Frame()
-      while(frame1.status<2){
-      frame1.getPoints(points)
-      frame1.getScoreType()}
-      score1.updateTotal(frame1.pointsTotal)
-  }
+      var frame1 = new Frame(stPrevFrame1, stPrevFrame2)
+      while (frame1.status < 2) {
+
+        //println(stPrevFrame1 + "  " + stPrevFrame2)
+        frame1.getPoints(points)
+
+        frame1.getScoreType(points)
+        stPrevFrame2 = stPrevFrame1
+
+        stPrevFrame1 = frame1.scoreType
+
+        score1.updateTotal(frame1.pointsTotal)
+
+      }
+    }
+
     println(score1.scoreTotal)
 
-}}
+  }
+}
 
 
 class Score(){
@@ -27,28 +40,41 @@ class Score(){
 
 }
 
-class Frame() {
+class Frame(statusPrevFrame1: String, statusPrevFrame2: String) {
   var pointsTotal: Int = 0
+  var statusPF1 = statusPrevFrame1
+  var statusPF2 = statusPrevFrame2
   var scoreType = "normal"
-  var points = 0
-  var status = 0
+  var pointsFrame: Int = 0
+  var status: Int = 0
 
   def getPoints(point: Int): Unit = {
-    points = point
+    if (statusPF1 =="strike" && statusPF2=="strike") {
+      pointsFrame = point * 3
+    } else if (statusPF1 =="strike" ){
+      pointsFrame = point * 2
+    }else{
+      pointsFrame = point
+    }
     status = status + 1
-    pointsTotal = pointsTotal + points
+    pointsTotal = pointsTotal + pointsFrame
 
   }
 
-  def getScoreType(): Any = {
+  def getScoreType(point: Int): Any = {
+    val points = point
     if (points == 10) {
       scoreType = "strike"
       status = 2
+      //println("first")
     } else if (pointsTotal == 10 && status == 2) {
       scoreType = "spare"
+      //println("second")
     } else {
       scoreType = "normal"
+      //println("third")
     }
+    scoreType
 
   }
 }
--- src/test/scala/BowlingTest.scala
+++ src/test/scala/BowlingTest.scala
@@ -2,24 +2,28 @@ import org.scalatest.FunSuite
 
 class BowlingTest extends FunSuite {
   test("one frame resulting in a spare") {
-    var frame1 = new Frame()
+    var stPrevFrame1 = "normal"
+    var stPrevFrame2 = "normal"
+    var frame1 = new Frame(stPrevFrame1, stPrevFrame2)
     frame1.getPoints(8)
     assert(frame1.pointsTotal === 8)
     assert(frame1.scoreType === "normal")
     frame1.getPoints(2)
-    frame1.getScoreType()
+    frame1.getScoreType(2)
     assert(frame1.pointsTotal === 10)
     assert(frame1.scoreType === "spare")
     assert(frame1.status === 2)
   }
 
   test("one frame, only normal shots") {
-    var frame1 = new Frame()
+    var stPrevFrame1 = "normal"
+    var stPrevFrame2 = "normal"
+    var frame1 = new Frame(stPrevFrame1, stPrevFrame2)
     frame1.getPoints(4)
     assert(frame1.pointsTotal === 4)
     assert(frame1.scoreType === "normal")
     frame1.getPoints(2)
-    frame1.getScoreType()
+    frame1.getScoreType(2)
     assert(frame1.pointsTotal === 6)
     assert(frame1.scoreType === "normal")
     assert(frame1.status === 2)
