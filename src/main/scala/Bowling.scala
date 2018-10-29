d0bad24446d3abe94a1285046688b00997e0ae47
bettyES <betty.schirrmeister@gmail.com>
2018-10-29 18:24:03 +0000

    added a frame class

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -1,23 +1,44 @@
 object Bowling{
 
   def main(args: Array[String]): Unit = {
+    //points = scala.io.StdIn.readInt()
 
+    var points = 10
 
   }
 
 }
 
-class Score(){
-  var pointsTotal:Int = 0
-  var typeScore = "normal"
 
-  def updateScore(points: Int): Int ={
-    pointsTotal = pointsTotal + points
-    pointsTotal
+class Score(){
+  var scoreTotal:Int = 0
+
+  def updateTotal(pointsFrame: Int){
+  scoreTotal = scoreTotal + pointsFrame
+    scoreTotal
   }
 
-  //    def updateScopreType(points: Int): String ={
-  //
-  //    }
+}
 
+class Frame() {
+  var pointsTotal: Int = 0
+  var scoreType = "normal"
+  var points = 0
+  var status = 0
+
+  def getPoints(points: Int): Unit = {
+    status = status + 1
+    pointsTotal = pointsTotal + points
+  }
+
+  def getScoreType(): Any = {
+    if (points == 10) {
+      scoreType = "strike"
+    } else if (pointsTotal == 10 & status == 2) {
+      scoreType = "spare"
+    } else {
+      scoreType = "normal"
+    }
+
+  }
 }
--- src/test/scala/BowlingTest.scala
+++ src/test/scala/BowlingTest.scala
@@ -2,7 +2,7 @@ import org.scalatest.FunSuite
 
 class BowlingTest extends FunSuite{
   test("all normal one score") {
-    var score1 = new Score()
-    score1.updateScore(8)
-    assert(score1.pointsTotal === 8)}
+    var frame1 = new Frame()
+    frame1.getPoints(8)
+    assert(frame1.pointsTotal === 8)}
 }
